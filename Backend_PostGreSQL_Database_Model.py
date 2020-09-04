0c09a0f7688e8415d1a9f73c94407e011effd967
Sneha <sneha46749@gmail.com>
2020-09-05 00:00:01 +0530

    Storing the values into a PostGreSQL database

---
+++ Backend_PostGreSQL_Database_Model.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/height_collector'  
+#Connectng the database with the app
+db = SQLAlchemy()   #Creating a class alchemy object for this falsk application
+
+class Data(db.Model):
+    __tablename__="data"
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(120),unique=True)
+    height = db.Column(db.Integer)
+
+"""Initialising the class variables/instance variables. __init__ is the first method
+ to be called when calling a class"""
+def __init__(self,email,height):
+    self.email = email
+    self.height = height
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success",methods=['POST'])   #POST is the second input that is entered by user
+def success():
+    if request.method=='POST':
+        email = request.form["email_name"]
+        height = request.form["height"]
+        print(email,height)
+        return render_template("success.html")    
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run() 
+
