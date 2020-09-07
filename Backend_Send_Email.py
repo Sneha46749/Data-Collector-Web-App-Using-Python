6d328e5d51b99ff85c90c35267973dd8681fc834
Sneha <sneha46749@gmail.com>
2020-09-07 23:48:05 +0530

    send_email function defined which is used to send the email to the user

--- Backend_Send_Email.py
+++ Backend_Send_Email.py
@@ -1,13 +1,13 @@
 from email.mime.text import MIMEText
 import smtplib
 
-def send_email(email,height):
+def send_email(email,height,average_height,count):
     from_email="sneha46749@gmail.com"
     from_password="Sneha"
     to_email=email
 
     subject="Height Data"
-    message="Hey there! Your height is <strong>%s</strong>." % height
+    message="Hey there! Your height is <strong>%s</strong>.<br>Average height is <strong>%s</strong> and that is calculated out of <strong>%s</strong> people.<br> Thanks!" % (height,average_height,count)
 
     msg=MIMEText(message,'html')
     msg['Subject']=subject
